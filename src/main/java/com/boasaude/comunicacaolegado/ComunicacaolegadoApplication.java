6073495a77c83f0e771bfb3eeffd1aa6a19d6a43
daniel.zat <daniel.zat@cwi.com.br>
2022-02-15 00:26:38 -0300

    Commit inicial

---
+++ src/main/java/com/boasaude/comunicacaolegado/ComunicacaolegadoApplication.java
@@ -0,0 +1,13 @@
+package com.boasaude.comunicacaolegado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicacaolegadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunicacaolegadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boasaude/comunicacaolegado/ComunicacaolegadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boasaude.comunicacaolegado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicacaolegadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
